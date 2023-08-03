9abfe132bde75e857ad220450c2690cc4d233557
PeetaniVijayaBhavani <125951404+PeetanivijayaBhavani@users.noreply.github.com>
2023-08-03 10:46:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+// import logo from './logo.svg';
+import './App.css';
+// import ClickCounter from './component/ClickCounter';
+// import HoverCount from './component/HoverCount';
+ import Zomato from './component/zomato';
+//  import Emoji from './component/Emoji';
+// import E-commerse from './component/E-commerse';
+// import Greet from './component/Greet';
+// import Calc from './calc_fol/Calc';
+// import Welcome from './component/Welcome1';
+// import BMICalculator from './bmi-fol/BMICalculator';
+// import Functionclick from './component/Functionclick';
+// import Classclick from './component/Classclick';
+// import Nameless from './component/Nameless';
+// import Emojipicker from 'emoji-picker-react'
+function App() {
+  return (
+    <div className="App">
+      
+    {/* <Greet name="Bhavani" heroName="Devarakonda"/>
+    <Greet name="peetani"/>
+    <Greet name="shiney"/> */}
+        {/* <Welcome name="vijay"/> */}
+      {/* <Functionclick/>
+      <Classclick/> */}
+      {/* <Emoji/> */}
+      {/* <E-commerse/> */}
+      <Zomato/>
+      {/* <ClickCounter/>
+      <HoverCount/> */}
+    </div>
+  );
+}
+
+export default App;
